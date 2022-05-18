d7319628cd9d13b206c29a964ed92aa010ce7dd0
Batuhan Baybaş <b.baybas@gmail.com>
2022-05-18 23:49:49 +0300

    crate async thunk for http request

--- src/Redux/features/filmSlice.js
+++ src/Redux/features/filmSlice.js
@@ -1,12 +1,35 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {Api} from "../../Common/Apı/Api";
+
+export const getFilms = createAsyncThunk("getFilms", async (title, page) => {
+    const response = await Api.get(`s=${title}&page=${page}`);
+
+    return response.data;
+})
+
 
 const filmSlice = createSlice({
     name: 'film',
     initialState: {
         films: [],
         status: "idle",
+        page: 1,
         error: null
     },
+    extraReducers: {
+        [getFilms.pending]: (state, action) => {
+            state.status = "pending";
+        },
+        [getFilms.fulfilled]: (state, action) => {
+            state.films = action.payload;
+            state.status = "succeeded";
+            state.page = action.payload.page;
+        },
+        [getFilms.rejected]: (state, action) => {
+            state.status = "failed";
+            state.error = action.error.message;
+        }
+    }
 
 
 })
